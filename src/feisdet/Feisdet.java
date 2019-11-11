9048c3f853e7b49928737e298c94dca2270a93de
madonna reda <50421738+madonnasheko@users.noreply.github.com>
2019-11-11 22:02:16 +0200

    second commit

---
+++ src/feisdet/Feisdet.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package feisdet;
+
+/**
+ *
+ * @author Akram
+ */
+public class Feisdet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       //{{'a','k','r'},{'s','b','r'}};
+       
+       //String xs = x[0].toString();
+       int a = 'a' ^ 'f';
+       System.out.println(a);
+    }
+    
+}
